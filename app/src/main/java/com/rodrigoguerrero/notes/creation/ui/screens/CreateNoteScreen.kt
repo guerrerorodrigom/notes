0f33f068042d7614819f1370eeeb4045ca690afe
guerrerorodrigom <guerrerorodrigom@gmail.com>
2022-01-07 15:30:07 +0100

    Remove FAB from create notes.

--- app/src/main/java/com/rodrigoguerrero/notes/creation/ui/screens/CreateNoteScreen.kt
+++ app/src/main/java/com/rodrigoguerrero/notes/creation/ui/screens/CreateNoteScreen.kt
@@ -1,61 +1,51 @@
 package com.rodrigoguerrero.notes.creation.ui.screens
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.activity.compose.BackHandler
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Archive
-import androidx.compose.material.icons.filled.Save
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.rodrigoguerrero.notes.R
 import com.rodrigoguerrero.notes.common.ui.components.BackNavigationIcon
 import com.rodrigoguerrero.notes.common.ui.components.FulLScreenProgress
 import com.rodrigoguerrero.notes.common.ui.components.MainScaffold
-import com.rodrigoguerrero.notes.common.ui.fabBottomPadding
 import com.rodrigoguerrero.notes.common.ui.topBarElevation
 import com.rodrigoguerrero.notes.creation.ui.components.EditNoteFields
 import com.rodrigoguerrero.notes.creation.ui.components.EditNoteTopAppBarActions
 import com.rodrigoguerrero.notes.creation.viewmodels.CreateNoteViewModel
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun CreateNoteScreenContent(
     onNavigateBack: () -> Unit
 ) {
     val viewModel: CreateNoteViewModel = hiltViewModel()
-    val isFabEnabled by viewModel.isFabEnabled.collectAsState(false)
     val showProgress by viewModel.processing.collectAsState(false)
     val isNoteSaved by viewModel.successState.collectAsState(false)
+    val title by viewModel.title.collectAsState()
+    val content by viewModel.content.collectAsState()
+    val scope = rememberCoroutineScope()
 
     MainScaffold(
-        fab = {
-            if (isFabEnabled) {
-                FloatingActionButton(
-                    onClick = {
-                        viewModel.createNote()
-                    },
-                    contentColor = MaterialTheme.colors.onSurface,
-                    modifier = Modifier.padding(bottom = fabBottomPadding)
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Save,
-                        contentDescription = stringResource(R.string.save_note)
-                    )
-                }
-            }
-        },
         topBar = {
             TopAppBar(
                 elevation = topBarElevation,
                 backgroundColor = MaterialTheme.colors.primarySurface,
-                navigationIcon = { BackNavigationIcon(onNavigateBack) },
-                actions = { EditNoteTopAppBarActions(
-                    archiveIcon = Icons.Filled.Archive,
-                    onArchiveUnarchive = { }
-                ) },
+                navigationIcon = {
+                    BackNavigationIcon {
+                        viewModel.createNote()
+                        onNavigateBack()
+                    }
+                },
+                actions = {
+                    EditNoteTopAppBarActions(
+                        archiveIcon = Icons.Filled.Archive,
+                        onArchiveUnarchive = { }
+                    )
+                },
                 title = {}
             )
         }
@@ -63,26 +53,45 @@ fun CreateNoteScreenContent(
         when {
             isNoteSaved -> onNavigateBack()
             showProgress -> FulLScreenProgress()
-            else -> CreateNote(viewModel)
+            else -> CreateNote(
+                title = title,
+                content = content,
+                scope = scope,
+                onTitleChanged = { viewModel.onTitleChanged(it) },
+                onContentChanged = { viewModel.onContentChanged(it) },
+                onBackPressed = {
+                    viewModel.createNote()
+                    onNavigateBack()
+                }
+            )
         }
     }
 }
 
 @Composable
-fun CreateNote(viewModel: CreateNoteViewModel) {
-    val title by viewModel.title.collectAsState()
-    val content by viewModel.content.collectAsState()
-    val scope = rememberCoroutineScope()
+fun CreateNote(
+    title: String,
+    content: String,
+    scope: CoroutineScope,
+    onTitleChanged: (String) -> Unit,
+    onContentChanged: (String) -> Unit,
+    onBackPressed: () -> Unit
+) {
+
     val focusRequester = FocusRequester()
 
     EditNoteFields(
         title = title,
-        onTitleChanged = { viewModel.title.value = it },
+        onTitleChanged = { onTitleChanged(it) },
         content = content,
-        onContentChanged = { viewModel.content.value = it },
+        onContentChanged = { onContentChanged(it) },
         scope = scope,
         focusRequester = focusRequester
     )
+
+    BackHandler {
+        onBackPressed()
+    }
 
     DisposableEffect(Unit) {
         focusRequester.requestFocus()
--- app/src/main/java/com/rodrigoguerrero/notes/creation/viewmodels/CreateNoteViewModel.kt
+++ app/src/main/java/com/rodrigoguerrero/notes/creation/viewmodels/CreateNoteViewModel.kt
@@ -6,9 +6,7 @@ import com.rodrigoguerrero.notes.common.models.Note
 import com.rodrigoguerrero.notes.creation.repository.NoteCreationRepository
 import com.rodrigoguerrero.notes.creation.repository.NoteOperationStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
@@ -17,36 +15,54 @@ import javax.inject.Inject
 class CreateNoteViewModel @Inject constructor(
     private val noteCreationRepository: NoteCreationRepository
 ) : ViewModel() {
-
+    private val noChanges = MutableStateFlow(false)
     val successState = noteCreationRepository
         .noteOperationStatus
-        .map { it == NoteOperationStatus.Success }
+        .combine(noChanges) { status, noChanges ->
+            status == NoteOperationStatus.Success || noChanges
+        }
         .onEach {
-            title.value = ""
-            content.value = ""
+            _title.value = ""
+            _content.value = ""
         }
 
-    val title = MutableStateFlow("")
-    val content = MutableStateFlow("")
-    val isFabEnabled = content.map { it.isNotEmpty() }
+    private val _title = MutableStateFlow("")
+    val title: StateFlow<String>
+        get() = _title
+    private val _content = MutableStateFlow("")
+    val content: StateFlow<String>
+        get() = _content
+
     val processing = noteCreationRepository
         .noteOperationStatus
         .map {
             it == NoteOperationStatus.Processing
         }
 
+    fun onTitleChanged(title: String) {
+        _title.value = title
+    }
+
+    fun onContentChanged(content: String) {
+        _content.value = content
+    }
+
     fun createNote() {
         viewModelScope.launch {
-            val note = Note(
-                title = title.value,
-                content = content.value, // TODO: can i use flows here?
-                createdDate = Date().time,
-                modifiedDate = Date().time,
-                isHidden = false,
-                isArchived = false,
-                id = UUID.randomUUID()
-            )
-            noteCreationRepository.upsertTextNote(note)
+            if (_title.value.isNotEmpty() || _content.value.isNotEmpty()) {
+                val note = Note(
+                    title = _title.value,
+                    content = _content.value,
+                    createdDate = Date().time,
+                    modifiedDate = Date().time,
+                    isHidden = false,
+                    isArchived = false,
+                    id = UUID.randomUUID()
+                )
+                noteCreationRepository.upsertTextNote(note)
+            } else {
+                noChanges.emit(true)
+            }
         }
     }
 }
